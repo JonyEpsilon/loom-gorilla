dbdd77076274c4c5672dd1891b68d7952eccc530
JonyEpsilon <github@j-star.org>
2014-06-10 09:21:59 +0100

    Prepare for release w/ loom 0.5.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject loom-gorilla "0.1.0-SNAPSHOT"
+(defproject loom-gorilla "0.1.0"
   :description "A Gorilla REPL renderer for loom graphs."
   :url "https://github.com/JonyEpsilon/loom-gorilla"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [aysylu/loom "0.4.3-SNAPSHOT"]
+                 [aysylu/loom "0.5.0"]
                  [gorilla-renderable "1.0.0"]
                  [org.clojure/data.codec "0.1.0"]])
